d76d49b02c553e7877045dec7b076c4664f78942
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2026-03-02 23:38:19 +0530

    Update eval.py

--- plugin/eval.py
+++ plugin/eval.py
@@ -5,17 +5,17 @@ import io
 import asyncio
 
 @astra_command(name="eval")
-async def cpp_handler(okbclient, message):
+async def eval_cmd(client, message):
 
     if message.sender_id not in SUDO_USERS:
-        return await message.reply("❌ You are not allowed to use this command.")
+        return await message.reply("❌ Not Allowed")
 
-    code = message.text.split(maxsplit=1)
-    await smart_reply("running")
-    if len(code) < 2:
-        return await message.reply("Usage: `.cpp print('hello')`")
+    args = message.text.split(maxsplit=1)
 
-    cmd = code[1]
+    if len(args) < 2:
+        return await message.reply("Usage: .eval print('hi')")
+
+    code = args[1]
 
     old_stdout = sys.stdout
     old_stderr = sys.stderr
@@ -23,28 +23,24 @@ async def cpp_handler(okbclient, message):
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
 
-    async def aexec(code, event, client):
-        exec(
-            f'async def __aexec(event, client):\n'
-            + ''.join(f'\n {line}' for line in code.split('\n'))
-        )
-        return await locals()['__aexec'](event, client)
-
     try:
-        await aexec(cmd, message, client)
-        result = redirected_output.getvalue()
-        error = redirected_error.getvalue()
+        # async support
+        exec(
+            f'async def __aexec(client, message):\n'
+            + '\n'.join(f'    {line}' for line in code.split('\n'))
+        )
 
-        if error:
-            await message.reply(f"❌ Error:\n`{error}`")
-        elif result:
-            await message.reply(f"✅ Output:\n`{result}`")
-        else:
-            await message.reply("✅ Code Executed Successfully")
+        await locals()["__aexec"](client, message)
+
+        result = redirected_output.getvalue()
 
     except Exception:
-        await message.reply(f"❌ Exception:\n`{traceback.format_exc()}`")
+        result = traceback.format_exc()
 
-    finally:
-        sys.stdout = old_stdout
-        sys.stderr = old_stderr
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    if not result:
+        result = "✅ Done"
+
+    await message.reply(f"```\n{result}\n```")
