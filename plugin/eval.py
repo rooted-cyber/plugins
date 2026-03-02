e0133abdcd8bb793833ba681f6e87c4a81e023f9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2026-03-02 23:45:46 +0530

    Update eval.py

--- plugin/eval.py
+++ plugin/eval.py
@@ -2,7 +2,6 @@ from . import *
 import traceback
 import sys
 import io
-import asyncio
 
 @astra_command(name="eval")
 async def eval_cmd(client, message):
@@ -10,37 +9,25 @@ async def eval_cmd(client, message):
     if message.sender_id not in SUDO_USERS:
         return await message.reply("❌ Not Allowed")
 
-    args = message.text.split(maxsplit=1)
+    cmd = message.text.split(" ", 1)
 
-    if len(args) < 2:
+    if len(cmd) < 2:
         return await message.reply("Usage: .eval print('hi')")
 
-    code = args[1]
+    code = cmd[1]
 
     old_stdout = sys.stdout
-    old_stderr = sys.stderr
-
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
+    sys.stdout = io.StringIO()
 
     try:
-        # async support
-        exec(
-            f'async def __aexec(client, message):\n'
-            + '\n'.join(f'    {line}' for line in code.split('\n'))
-        )
-
-        await locals()["__aexec"](client, message)
-
-        result = redirected_output.getvalue()
-
+        exec(code)
+        output = sys.stdout.getvalue()
     except Exception:
-        result = traceback.format_exc()
+        output = traceback.format_exc()
 
     sys.stdout = old_stdout
-    sys.stderr = old_stderr
 
-    if not result:
-        result = "✅ Done"
+    if not output:
+        output = "✅ Done"
 
-    await message.reply(f"```\n{result}\n```")
+    await message.reply(f"```\n{output}\n```")
