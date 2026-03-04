3c3ab62d11760671e4e57be4042b9be013f64bb3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2026-03-04 20:29:28 +0530

    Add files via upload

--- plugin/btn.py
+++ plugin/btn.py
@@ -1,59 +1,21 @@
+from telethon import Button, events
 from . import *
 import os
 
 @astra_command(name="btn")
-async def whtsp_bot(client, message):
+async def btn_handler(client, message):
 
-    await message.reply("⚙ Creating WhatsApp bot...")
-
+    # File create using with open
     code = """
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+from telethon import Button
 
-async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState("session")
-
-    const sock = makeWASocket({
-        auth: state,
-        printQRInTerminal: true
-    })
-
-    sock.ev.on("creds.update", saveCreds)
-
-    sock.ev.on("messages.upsert", async ({ messages }) => {
-        const msg = messages[0]
-        if (!msg.message) return
-
-        const text = msg.message.conversation
-
-        if (text === ".menu") {
-            await sock.sendMessage(msg.key.remoteJid, {
-                text: "Choose option 👇",
-                footer: "Astra Bot",
-                buttons: [
-                    { buttonId: "owner", buttonText: { displayText: "👑 Owner" }, type: 1 },
-                    { buttonId: "help", buttonText: { displayText: "📜 Help" }, type: 1 }
-                ],
-                headerType: 1
-            })
-        }
-    })
-}
-
-startBot()
+buttons = [
+    [Button.url("🌐 Google", "https://google.com")],
+    [Button.inline("⚡ Click Me", b"astra_click")]
+]
 """
 
-    # Create folder
-    os.makedirs("whtsp_bot", exist_ok=True)
-
-    # Write JS file
-    with open("whtsp_bot/index.js", "w") as f:
+    with open("buttons_data.py", "w") as f:
         f.write(code)
 
-    # Install dependency
-    #os.system("cd whtsp_bot && npm init -y")
-    #.system("cd whtsp_bot && npm install @whiskeysockets/baileys")
-
-    await message.reply("🚀 Starting WhatsApp bot... Scan QR in terminal.")
-
-    # Run bot
-    os.system("cd whtsp_bot && node index.js")
+    os.system("python but*")
