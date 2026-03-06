df0145c8a3a7ee7298adcc63948bc0a697c73539
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2026-03-06 19:25:03 +0530

    Update exit.py

--- plugin/exit.py
+++ plugin/exit.py
@@ -1,7 +1,8 @@
 import os
 from utils.plugin_utils import astra_command
 
-@astra_command("exit")
+@astra_command("e)
 async def exit_cmd(client, message):
     await message.reply("🚪 Astra exiting...")
+    """h"""
     os._exit(0)
